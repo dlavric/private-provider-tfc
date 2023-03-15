027d8337b66cfc9f820ed813007eae220f722ac7
dlavric <daniela@hashicorp.com>
2023-03-15 15:24:34 +0100

    added new files

---
+++ providers.tf
@@ -0,0 +1,12 @@
+# terraform {
+#   required_providers {
+#     vra = {
+#       source = "vmware/vra"
+#       version = "0.7.1"
+#     }
+#   }
+# }
+
+# provider "vra" {
+#   # Configuration options
+# }
